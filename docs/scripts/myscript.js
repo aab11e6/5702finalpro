753ef514d1c8dd815f1a4d030673fee8828ac702
aab11e6 <sh4477@columbia.edu>
2023-12-14 01:22:34 -0500

    edit d3

--- docs/scripts/myscript.js
+++ docs/scripts/myscript.js
@@ -1,1 +1,105 @@
-// add your JavaScript/D3 to this file
+// 假设您的数据格式如下
+// 数据格式: [{incident_hour_of_the_day: 1, incident_state: 'State1', fraud_reported: 'FraudYes', incident_severity: 'Major', count: 10}, ...]
+
+// 假设数据存储在一个名为data的变量中
+
+let data = [{"incident_hour_of_the_day":"0-5","incident_state":"NC","fraud_reported":"N","Major":2,"Minor":12,"Total":3,"Trivial":1},{"incident_hour_of_the_day":"0-5","incident_state":"NC","fraud_reported":"Y","Major":8,"Minor":2,"Total":0,"Trivial":0},{"incident_hour_of_the_day":"0-5","incident_state":"NY","fraud_reported":"N","Major":6,"Minor":23,"Total":12,"Trivial":10},{"incident_hour_of_the_day":"0-5","incident_state":"NY","fraud_reported":"Y","Major":7,"Minor":2,"Total":2,"Trivial":1},{"incident_hour_of_the_day":"0-5","incident_state":"OH","fraud_reported":"N","Major":0,"Minor":3,"Total":1,"Trivial":0},{"incident_hour_of_the_day":"0-5","incident_state":"OH","fraud_reported":"Y","Major":4,"Minor":0,"Total":0,"Trivial":0},{"incident_hour_of_the_day":"0-5","incident_state":"PA","fraud_reported":"N","Major":1,"Minor":1,"Total":1,"Trivial":1},{"incident_hour_of_the_day":"0-5","incident_state":"PA","fraud_reported":"Y","Major":0,"Minor":1,"Total":1,"Trivial":1},{"incident_hour_of_the_day":"0-5","incident_state":"SC","fraud_reported":"N","Major":7,"Minor":14,"Total":11,"Trivial":5},{"incident_hour_of_the_day":"0-5","incident_state":"SC","fraud_reported":"Y","Major":10,"Minor":2,"Total":3,"Trivial":1},{"incident_hour_of_the_day":"0-5","incident_state":"VA","fraud_reported":"N","Major":3,"Minor":9,"Total":7,"Trivial":2},{"incident_hour_of_the_day":"0-5","incident_state":"VA","fraud_reported":"Y","Major":2,"Minor":3,"Total":0,"Trivial":0},{"incident_hour_of_the_day":"0-5","incident_state":"WV","fraud_reported":"N","Major":6,"Minor":21,"Total":12,"Trivial":11},{"incident_hour_of_the_day":"0-5","incident_state":"WV","fraud_reported":"Y","Major":5,"Minor":1,"Total":2,"Trivial":1},{"incident_hour_of_the_day":"12-17","incident_state":"NC","fraud_reported":"N","Major":4,"Minor":4,"Total":6,"Trivial":1},{"incident_hour_of_the_day":"12-17","incident_state":"NC","fraud_reported":"Y","Major":5,"Minor":3,"Total":0,"Trivial":0},{"incident_hour_of_the_day":"12-17","incident_state":"NY","fraud_reported":"N","Major":5,"Minor":23,"Total":23,"Trivial":0},{"incident_hour_of_the_day":"12-17","incident_state":"NY","fraud_reported":"Y","Major":13,"Minor":2,"Total":3,"Trivial":0},{"incident_hour_of_the_day":"12-17","incident_state":"OH","fraud_reported":"N","Major":0,"Minor":3,"Total":1,"Trivial":0},{"incident_hour_of_the_day":"12-17","incident_state":"OH","fraud_reported":"Y","Major":2,"Minor":0,"Total":0,"Trivial":0},{"incident_hour_of_the_day":"12-17","incident_state":"PA","fraud_reported":"N","Major":2,"Minor":4,"Total":3,"Trivial":0},{"incident_hour_of_the_day":"12-17","incident_state":"PA","fraud_reported":"Y","Major":1,"Minor":0,"Total":0,"Trivial":0},{"incident_hour_of_the_day":"12-17","incident_state":"SC","fraud_reported":"N","Major":8,"Minor":24,"Total":18,"Trivial":4},{"incident_hour_of_the_day":"12-17","incident_state":"SC","fraud_reported":"Y","Major":17,"Minor":5,"Total":6,"Trivial":0},{"incident_hour_of_the_day":"12-17","incident_state":"VA","fraud_reported":"N","Major":5,"Minor":6,"Total":7,"Trivial":3},{"incident_hour_of_the_day":"12-17","incident_state":"VA","fraud_reported":"Y","Major":4,"Minor":0,"Total":2,"Trivial":0},{"incident_hour_of_the_day":"12-17","incident_state":"WV","fraud_reported":"N","Major":9,"Minor":17,"Total":23,"Trivial":0},{"incident_hour_of_the_day":"12-17","incident_state":"WV","fraud_reported":"Y","Major":4,"Minor":0,"Total":1,"Trivial":0},{"incident_hour_of_the_day":"18-23","incident_state":"NC","fraud_reported":"N","Major":6,"Minor":9,"Total":7,"Trivial":3},{"incident_hour_of_the_day":"18-23","incident_state":"NC","fraud_reported":"Y","Major":4,"Minor":2,"Total":1,"Trivial":0},{"incident_hour_of_the_day":"18-23","incident_state":"NY","fraud_reported":"N","Major":8,"Minor":23,"Total":26,"Trivial":0},{"incident_hour_of_the_day":"18-23","incident_state":"NY","fraud_reported":"Y","Major":13,"Minor":1,"Total":2,"Trivial":0},{"incident_hour_of_the_day":"18-23","incident_state":"OH","fraud_reported":"N","Major":1,"Minor":0,"Total":2,"Trivial":0},{"incident_hour_of_the_day":"18-23","incident_state":"OH","fraud_reported":"Y","Major":2,"Minor":0,"Total":0,"Trivial":0},{"incident_hour_of_the_day":"18-23","incident_state":"PA","fraud_reported":"N","Major":0,"Minor":0,"Total":2,"Trivial":1},{"incident_hour_of_the_day":"18-23","incident_state":"PA","fraud_reported":"Y","Major":0,"Minor":1,"Total":0,"Trivial":0},{"incident_hour_of_the_day":"18-23","incident_state":"SC","fraud_reported":"N","Major":7,"Minor":19,"Total":13,"Trivial":0},{"incident_hour_of_the_day":"18-23","incident_state":"SC","fraud_reported":"Y","Major":15,"Minor":0,"Total":1,"Trivial":0},{"incident_hour_of_the_day":"18-23","incident_state":"VA","fraud_reported":"N","Major":4,"Minor":9,"Total":8,"Trivial":1},{"incident_hour_of_the_day":"18-23","incident_state":"VA","fraud_reported":"Y","Major":3,"Minor":1,"Total":2,"Trivial":0},{"incident_hour_of_the_day":"18-23","incident_state":"WV","fraud_reported":"N","Major":7,"Minor":12,"Total":15,"Trivial":2},{"incident_hour_of_the_day":"18-23","incident_state":"WV","fraud_reported":"Y","Major":10,"Minor":0,"Total":3,"Trivial":0},{"incident_hour_of_the_day":"6-11","incident_state":"NC","fraud_reported":"N","Major":2,"Minor":9,"Total":2,"Trivial":5},{"incident_hour_of_the_day":"6-11","incident_state":"NC","fraud_reported":"Y","Major":3,"Minor":2,"Total":2,"Trivial":2},{"incident_hour_of_the_day":"6-11","incident_state":"NY","fraud_reported":"N","Major":4,"Minor":29,"Total":5,"Trivial":7},{"incident_hour_of_the_day":"6-11","incident_state":"NY","fraud_reported":"Y","Major":9,"Minor":2,"Total":1,"Trivial":0},{"incident_hour_of_the_day":"6-11","incident_state":"OH","fraud_reported":"N","Major":1,"Minor":0,"Total":0,"Trivial":1},{"incident_hour_of_the_day":"6-11","incident_state":"OH","fraud_reported":"Y","Major":1,"Minor":0,"Total":1,"Trivial":0},{"incident_hour_of_the_day":"6-11","incident_state":"PA","fraud_reported":"N","Major":2,"Minor":2,"Total":2,"Trivial":0},{"incident_hour_of_the_day":"6-11","incident_state":"PA","fraud_reported":"Y","Major":2,"Minor":1,"Total":0,"Trivial":0},{"incident_hour_of_the_day":"6-11","incident_state":"SC","fraud_reported":"N","Major":4,"Minor":14,"Total":14,"Trivial":13},{"incident_hour_of_the_day":"6-11","incident_state":"SC","fraud_reported":"Y","Major":10,"Minor":3,"Total":0,"Trivial":0},{"incident_hour_of_the_day":"6-11","incident_state":"VA","fraud_reported":"N","Major":3,"Minor":9,"Total":6,"Trivial":3},{"incident_hour_of_the_day":"6-11","incident_state":"VA","fraud_reported":"Y","Major":5,"Minor":2,"Total":1,"Trivial":0},{"incident_hour_of_the_day":"6-11","incident_state":"WV","fraud_reported":"N","Major":2,"Minor":17,"Total":14,"Trivial":10},{"incident_hour_of_the_day":"6-11","incident_state":"WV","fraud_reported":"Y","Major":8,"Minor":2,"Total":2,"Trivial":0}]
+
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 750 - margin.left - margin.right,
+    height = 550 - margin.top - margin.bottom;
+// 设置SVG的尺寸和边距
+const svg = d3.select("div#plot").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+
+const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+
+// 定义比例尺和颜色
+const x = d3.scaleBand().rangeRound([0, width]).paddingInner(0.05).align(0.1),
+      y = d3.scaleLinear().rangeRound([height, 0]),
+      z = d3.scaleOrdinal().range(['#6b486b','#8a89a6','#98abc5','#ff8c00']);
+
+// 更新图表的函数
+function updateChart(selectedState, selectedFraud) {
+
+    const filteredData = data.filter(d => d.incident_state === selectedState && d.fraud_reported === selectedFraud);
+
+    // 数据转换为堆叠格式
+    const keys = [ 'Total','Major','Minor','Trivial']; // 根据您的数据调整
+    const stackData = d3.stack().keys(keys)(filteredData);
+
+    // 设置比例尺的域
+    x.domain(filteredData.map(d => d.incident_hour_of_the_day));
+    y.domain([0, d3.max(stackData, d => d3.max(d, d => d[1]))]).nice();
+    z.domain(keys);
+
+    // 清除旧的图表元素
+    g.selectAll("*").remove();
+
+    // 绘制条形图
+    g.append("g")
+      .selectAll("g")
+      .data(stackData)
+      .enter().append("g")
+        .attr("fill", d => z(d.key))
+      .selectAll("rect")
+      .data(d => d)
+      .enter().append("rect")
+        .attr("x", d => x(d.data.incident_hour_of_the_day))
+        .attr("y", d => y(d[1]))
+        .attr("height", d => y(d[0]) - y(d[1]))
+        .attr("width", x.bandwidth());
+
+    // 添加 x 轴
+    g.append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x));
+
+    // 添加 y 轴
+    g.append("g")
+        .call(d3.axisLeft(y));
+
+
+    // 添加图例
+    const legend = g.append("g")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 10)
+      .attr("text-anchor", "end")
+    .selectAll("g")
+    .data(keys.slice().reverse())
+    .enter().append("g")
+      .attr("transform", (d, i) => `translate(-20,${i * 20})`);
+
+    legend.append("rect")
+      .attr("x", width - 19)
+      .attr("width", 19)
+      .attr("height", 19)
+      .attr("fill", z);
+
+    legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9.5)
+      .attr("dy", "0.32em")
+      .text(d => d);
+}
+
+// 为单选按钮添加事件监听
+d3.selectAll('input[name="stateline"]').on('change', function() {
+  const state = d3.select(this).property("value");
+  const fraud = d3.select('input[name="fraudline"]:checked').property("value");
+  updateChart(state, fraud);
+});
+
+d3.selectAll('input[name="fraudline"]').on('change', function() {
+  const fraud = d3.select(this).property("value");
+  const state = d3.select('input[name="stateline"]:checked').property("value");
+  updateChart(state, fraud);
+});
+
+// 初始化图表
+const initState = d3.select('input[name="stateline"]:checked').property("value");
+const initFraud = d3.select('input[name="fraudline"]:checked').property("value");
+updateChart(initState, initFraud);
+
+
--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,1 +1,20 @@
 // add your JavaScript/D3 to this file
+
+  const w = 400;
+  const h = 300;
+  const margin = {top: 25, right: 0, bottom: 25,
+      left: 25};
+  const innerWidth = w - margin.left - margin.right;
+  const innerHeight = h - margin.top - margin.bottom;
+
+  const svg = d3.select("div#plot")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
+
+  svg.append("rect")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", w)
+      .attr("height", h)
+      .attr("fill", "lightblue");
